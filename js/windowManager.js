7e37547244f603eff85811aa53ff37f106d1ca6d
julianleiss <julian.leiss@gmail.com>
2024-05-06 06:37:55 -0300

    Update windowManager.js

--- js/windowManager.js
+++ js/windowManager.js
@@ -25,6 +25,29 @@ class WindowManager {
         });
         windowToFocus.element.style.zIndex = '10'; // Raise the z-index for the focused window
     }
+    makeDraggable(windowElement, titleBar) {
+        let startPosX = 0, startPosY = 0, drag = false;
+        titleBar.addEventListener('mousedown', (e) => {
+            if (e.target === titleBar) { // Asegura que solo se aplique al arrastrar la barra de título
+                this.windowManager.focusWindow(this);
+                startPosX = e.clientX - windowElement.offsetLeft;
+                startPosY = e.clientY - windowElement.offsetTop;
+                drag = true;
+            }
+        });
+    
+        document.addEventListener('mousemove', (e) => {
+            if (drag) {
+                windowElement.style.left = `${e.clientX - startPosX}px`;
+                windowElement.style.top = `${e.clientY - startPosY}px`;
+            }
+        });
+    
+        document.addEventListener('mouseup', () => {
+            drag = false;
+        });
+    }
 }
+
 
 export default new WindowManager();
