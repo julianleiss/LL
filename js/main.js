757972a880594b6676f206e164ecec72dcc26f6d
julianleiss <julian.leiss@gmail.com>
2024-05-06 06:37:05 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,34 +1,38 @@
+//main.js
 import windowManager from './windowManager.js';
 import Icon from './icon.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const desktop = document.getElementById('desktop'); // Asegúrate de que el contenedor 'desktop' esté definido en el HTML
     const iconsData = [
-        { id: '0001', imagePath: './images/image1.png' },
-        { id: '0002', imagePath: './images/image2.png' },
-        { id: '0003', imagePath: './images/image3.png' },
-        { id: '0004', imagePath: './images/image4.png' },
-        { id: '0005', imagePath: './images/image5.png' },
-        { id: '0006', imagePath: './images/image6.png' },
-        { id: '0007', imagePath: './images/image7.png' },
-        { id: '0008', imagePath: './images/image8.png' },
+        { id: '0001', imagePath: './images/image1.png', type: 'Mundillo1' },
+        { id: '0002', imagePath: './images/image2.png', type: 'Mundillo2' },
+        { id: '0001', imagePath: './images/image1b.png', type: 'Mundillo1' },
+        { id: '0002', imagePath: './images/image2b.png', type: 'Mundillo2' },
+        { id: '0003', imagePath: './images/image3.png', type: 'Mundillo3' },
+        { id: '0004', imagePath: './images/image4.png', type: 'Mundillo4' },
+        { id: '0003', imagePath: './images/image3b.png', type: 'Mundillo3' },
+        { id: '0004', imagePath: './images/image4b.png', type: 'Mundillo4' },
+        { id: '0005', imagePath: './images/image5.png', type: 'Mundillo5' },
+        { id: '0005', imagePath: './images/image5b.png', type: 'Mundillo5' },
+        { id: '0006', imagePath: './images/image6.png', type: 'Mundillo6' },
+        { id: '0007', imagePath: './images/image7.png', type: 'Mundillo7' },
+        { id: '0007', imagePath: './images/image7.png', type: 'Mundillo7' },
+        { id: '0008', imagePath: './images/image8.png', type: 'Mundillo8' },
+        { id: '0001', imagePath: './images/image1.png', type: 'Mundillo1' }
     ];
 
     iconsData.forEach(iconData => {
         const icon = new Icon(iconData.id, iconData.imagePath);
-        const iconElement = icon.createIconElement();
-        desktop.appendChild(iconElement);
+        document.body.appendChild(icon.element);
 
         // Posicionamiento aleatorio del icono
-        iconElement.style.left = `${Math.random() * (window.innerWidth - iconElement.offsetWidth)}px`;
-        iconElement.style.top = `${Math.random() * (window.innerHeight - iconElement.offsetHeight)}px`;
+        icon.element.style.left = `${Math.random() * (window.innerWidth - icon.width)}px`;
+        icon.element.style.top = `${Math.random() * (window.innerHeight - icon.height)}px`;
 
-        iconElement.addEventListener('click', () => {
-            const width = iconData.id === '0001' ? 500 : 400;
-            const height = iconData.id === '0001' ? 500 : 600;
-            const posX = Math.random() * (window.innerWidth - width);
-            const posY = Math.random() * (window.innerHeight - height);
-            windowManager.openWindow(iconData.id, `Mundillo ${iconData.id}`, width, height, posX, posY);
+        icon.element.addEventListener('click', () => {
+            const posX = Math.random() * (window.innerWidth - (iconData.type === "Mundillo1" ? 500 : 400));
+            const posY = Math.random() * (window.innerHeight - (iconData.type === "Mundillo" ? 500 : 600));
+            windowManager.openWindow(iconData.id, iconData.type, posX, posY);
         });
     });
 });
